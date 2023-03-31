1f7f2609a4a04031fd320247a673c2e9530e8a90
Pepe <pepoclesng@gmail.com>
2023-03-30 20:31:17 -0600

    nomas documentando el Score.cs que esta medio random

--- Score.cs
+++ Score.cs
@@ -3,25 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
+// Creo que esta clase se explica sola pero xD aqui ando
 public class Score : MonoBehaviour
 {
-    public static int _Score;
+  // Se inicializan variables
+  public static int _Score;
 
-    public int ScorePerShot = 5;
+  public int ScorePerShot = 5;
 	public TxtMng _TxtMng;
 
+  // Esta funcion es un poco random pero ps asi venia
 	void Start(){
 		ClearScore();
 	}
 
-    public void ClearScore(){
-	    _Score = 0;
-		_TxtMng.SetScoreText(_Score);
+  // Regresamos el valor de _Score a 0 y actualizamos el texto que lo muestra
+  public void ClearScore(){
+    _Score = 0;
+    _TxtMng.SetScoreText(_Score);
 	}
 
+  // actualizamos el valor de _Score y el texto que lo muestra
 	public void AddScore(){
-	    _Score += ScorePerShot;
+    _Score += ScorePerShot;
 		_TxtMng.SetScoreText(_Score);
 	}
-	
 }
